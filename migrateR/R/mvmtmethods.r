a6ba5dee22e088caa83f668c3c9d7d63233a4346
Derek B Spitz <spitz.derek@gmail.com>
2019-11-13 11:48:54 -0800

    close )

--- migrateR/R/mvmtmethods.r
+++ migrateR/R/mvmtmethods.r
@@ -180,7 +180,7 @@
 	  points(x1, y1, cex = .5, col = "grey", xlim = xlim)
 	  if(max(xlim)>max(x1))  warning(cat("some x-values > xlim; n ="
 	                                     , sum(x1>max(xlim))
-	                                     , "points beyond plotting range")
+	                                     , "points beyond plotting range"))
 
       # Omit Models From Plot (?)
       omit <- match(omit,names(mvmt@models))
